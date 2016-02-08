4fa88f4cbe07f2037d3e55afcdfea97089584f9d
JeanPascalEvette <jp.evette1@gmail.com>
2016-02-08 20:53:41 +0000

    Adding tool to setup spring variables

--- Assets/Scripts/CarProperties.cs
+++ Assets/Scripts/CarProperties.cs
@@ -4,30 +4,46 @@ using System.Collections;
 public class CarProperties : MonoBehaviour {
 
     private GameObject carBody;
-    public float springJoinDistance = 2.0f;
     public float carSpeed = 50.0f;
     public float carRotation = 20.0f;
-    public float motorTargetVelocity = 100.0f;
-    public float motorForce = 50.0f;
+
+    public float springSpring;
+    public float springDamper;
+    public float springMinDistance;
+    public float springMaxDistance;
+    public float springTolerance;
+    public float carBodyMass;
+    public float wheelsMass;
+
     // Use this for initialization
     void Start () {
         carBody = this.transform.Find("Body").gameObject;
-        JointMotor newMotor = new JointMotor();
-        //newMotor.targetVelocity = -motorTargetVelocity;
-        //newMotor.force = motorForce;
-        newMotor.freeSpin = true;
 
-        JointMotor newMotor2 = new JointMotor();
-        //newMotor2.targetVelocity = motorTargetVelocity;
-        //newMotor2.force = motorForce;
-        newMotor2.freeSpin = true;
+        var springs = carBody.GetComponents<SpringJoint>();
 
-        var hinge1 = transform.Find("Motion Wheels").GetComponents<HingeJoint>()[0];
-        var hinge2 = transform.Find("Motion Wheels").GetComponents<HingeJoint>()[1];
+        foreach(SpringJoint spring in springs)
+        {
+            spring.spring = springSpring;
+            spring.damper = springDamper;
+            spring.minDistance = springMinDistance;
+            spring.maxDistance= springMaxDistance;
+            spring.tolerance = springTolerance;
+        }
 
-        hinge1.motor = newMotor;
-        hinge2.motor = newMotor2;
+        carBody.GetComponent<Rigidbody>().mass = carBodyMass;
 
+        var wheelsF = transform.Find("Direction Wheels").GetComponentsInChildren<Rigidbody>();
+        var wheelsB = transform.Find("Motion Wheels").GetComponentsInChildren<Rigidbody>();
+
+
+        foreach (Rigidbody rb in wheelsF)
+        {
+            rb.mass = wheelsMass;
+        }
+        foreach (Rigidbody rb in wheelsB)
+        {
+            rb.mass = wheelsMass;
+        }
     }
 	
 	// Update is called once per frame
