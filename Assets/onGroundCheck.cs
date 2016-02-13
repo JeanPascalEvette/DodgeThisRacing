ff3831871529727277ac33cf5417573e52ba866f
JeanPascalEvette <jp.evette1@gmail.com>
2016-02-13 17:43:56 +0000

    Importing Stefanos' project

---
+++ Assets/onGroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onGroundCheck : MonoBehaviour {
+
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	// sloppy way to check if wheels are on ground
+	void OnCollisionStay(Collision other){
+		firstMovement.onGround = true;
+	}
+
+	void OnCollisionExit(Collision other){
+
+		firstMovement.onGround = false;
+	}
+}
