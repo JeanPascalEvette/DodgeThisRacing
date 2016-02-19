8fc78d7f69fda6f4e10827f838c623ad99c06dd7
Jean-Pascal Evette(Laptop) <jp.evette1@gmail.com>
2016-02-19 13:01:40 +0000

    Implemented Center of mass & Mass on axles

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -13,6 +13,17 @@ public class CarController : MonoBehaviour {
     private float mCRolRes = 12.8f;
     [SerializeField]
     private float mCBrake = 3.0f;
+    [SerializeField]
+    private Transform RearLeftWheel;
+    [SerializeField]
+    private Transform RearRightWheel;
+    [SerializeField]
+    private Transform FrontLeftWheel;
+    [SerializeField]
+    private Transform FrontRightWheel;
+    [SerializeField]
+    private Vector3 CenterOfGravity = new Vector3(0.2f,0.5f,0);
+
 
 
 
@@ -22,22 +33,35 @@ public class CarController : MonoBehaviour {
     private Rigidbody rb;
 
     [SerializeField]
-    private Vector3 tractionForce;
+    private Vector3 TractionForce;
     [SerializeField]
-    private Vector3 dragForce;
+    private Vector3 DragForce;
     [SerializeField]
-    private Vector3 rollingResistance;
+    private Vector3 RollingResistance;
     [SerializeField]
-    private Vector3 longtitudinalForce;
-
+    private Vector3 LongtitudinalForce;
+    [SerializeField]
+    private Vector3 Acceleration;
+    [SerializeField]
+    private float WeightOnFrontWheels;
+    [SerializeField]
+    private float WeightOnRearWheels;
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
 
     }
 	
-	// Update is called once per frame
-	void Update ()
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position+CenterOfGravity, 0.1f);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         direction = 0.0f;
         if (Input.GetKey(KeyCode.W))
@@ -45,18 +69,29 @@ public class CarController : MonoBehaviour {
         else if (Input.GetKey(KeyCode.S))
             direction = -1.0f;
         if (direction >= 0)
-            tractionForce = transform.forward * direction * mEngineForce;
+            TractionForce = transform.forward * direction * mEngineForce;
         else
-            tractionForce = transform.forward * -mCBrake;
-        var speed = Mathf.Sqrt(tractionForce.x * tractionForce.x + tractionForce.y * tractionForce.y);
-        dragForce = new Vector3(-mCDrag * tractionForce.x * speed, -mCDrag * tractionForce.y * speed, 0);
-        rollingResistance = -mCRolRes * tractionForce;
+            TractionForce = transform.forward * -mCBrake;
+        var speed = Mathf.Sqrt(TractionForce.x * TractionForce.x + TractionForce.z * TractionForce.z);
+        DragForce = new Vector3(-mCDrag * TractionForce.x * speed, -mCDrag * TractionForce.z * speed, 0);
+        RollingResistance = -mCRolRes * TractionForce;
 
 
-        longtitudinalForce = tractionForce + dragForce + rollingResistance;
-        var acceleration = longtitudinalForce / rb.mass;
+        LongtitudinalForce = TractionForce + DragForce + RollingResistance;
+        Acceleration = LongtitudinalForce / rb.mass;
 
-        rb.velocity = rb.velocity + Time.deltaTime * acceleration;
+        rb.velocity = rb.velocity + Time.deltaTime * Acceleration;
 
+
+        WeightOnFrontWheels = GetMassOnAxle(FrontRightWheel.localPosition.z) - (CenterOfGravity.y / (FrontRightWheel.localPosition.z - RearRightWheel.localPosition.z)) * rb.mass * Acceleration.x;
+        WeightOnRearWheels = GetMassOnAxle(RearRightWheel.localPosition.z) + (CenterOfGravity.y / (FrontRightWheel.localPosition.z - RearRightWheel.localPosition.z)) * rb.mass * Acceleration.x;
+
+    }
+
+    public float GetMassOnAxle(float zCoord)
+    {
+        float distance = Mathf.Abs(zCoord - CenterOfGravity.z);
+        float wheelDist = RearRightWheel.localPosition.z - FrontRightWheel.localPosition.z;
+        return (distance / wheelDist) * rb.mass;
     }
 }
--- Assets/WheelController.cs
+++ Assets/WheelController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class WheelController : MonoBehaviour {
 
+    [SerializeField]
+    private float mFrictionCoef = 1.0f;
+
+    private CarController mCarController;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        mCarController = transform.parent.GetComponent<CarController>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    public float GetFrictionLimit()
+    {
+        return mFrictionCoef * mCarController.GetMassOnAxle(transform.localPosition.z);
+    }
 }
