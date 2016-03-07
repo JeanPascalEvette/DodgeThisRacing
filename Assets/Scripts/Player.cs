4fd186bfaac1bb5cd6658e423a829c0a504b7309
Federico Soncini <fsonc002@gold.ac.uk>
2016-03-07 18:53:12 +0000

    Added 'X' button
    Locks player to Car Selection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,16 +16,25 @@ public class Player : MonoBehaviour {
 
     }
 
+
     float move_player = 5.0f;
 
+    public bool notSelected = true;
     //Function to move the basket
     void HandleMovement()
     {
-        if (Input.GetKey(KeyCode.RightArrow)) { transform.Translate(move_player, 0, 0); }
-        if (Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(-move_player, 0, 0); }
-        if (Input.GetKey(KeyCode.UpArrow)) { transform.Translate(0, move_player, 0); }
-        if (Input.GetKey(KeyCode.DownArrow)) { transform.Translate(0, -move_player, 0); }
+        if (notSelected)
+        {
+            if (Input.GetKey(KeyCode.RightArrow)) { transform.Translate(move_player, 0, 0); }
+            if (Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(-move_player, 0, 0); }
+            if (Input.GetKey(KeyCode.UpArrow)) { transform.Translate(0, move_player, 0); }
+            if (Input.GetKey(KeyCode.DownArrow)) { transform.Translate(0, -move_player, 0); }
+        }
+        
+
     }
+
+
 
     //void OnCollisionEnter2D(Collision2D col)
     //{
