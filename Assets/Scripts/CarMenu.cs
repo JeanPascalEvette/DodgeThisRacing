116c05ceec67e6d334e63de509f4fe6f559e7917
Elio de Berardinis <eliodeb@gmail.com>
2016-03-07 18:12:36 +0000

    adding color change when collision detected

--- Assets/Scripts/CarMenu.cs
+++ Assets/Scripts/CarMenu.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class CarMenu : MonoBehaviour {
 
-    
+
+    public Text TextColor;
+
+    void Start()
+    {
+        TextColor = GetComponent<Text>();
+    }
+
     void OnTriggerEnter2D (Collider2D trigger)
     {
         print("Trigger");
+
+        TextColor.color = Color.yellow;
+
     }
     
     void OnTriggerExit2D(Collider2D trigger)
     {
         print("EXIT");
+        TextColor.color = Color.white;
     }
 
 }
