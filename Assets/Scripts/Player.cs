2397f497a143019763feca06b0f86de7950ff313
Federico Soncini <fsonc002@gold.ac.uk>
2016-03-07 15:07:40 +0000

    Added Colliders to Car and Player menu items
    test

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        HandleMovement();
+
+    }
+
+    float move_player = 5.0f;
+
+    //Function to move the basket
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) { transform.Translate(move_player, 0, 0); }
+        if (Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(-move_player, 0, 0); }
+        if (Input.GetKey(KeyCode.UpArrow)) { transform.Translate(0, move_player, 0); }
+        if (Input.GetKey(KeyCode.DownArrow)) { transform.Translate(0, -move_player, 0); }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+
+        if (col.gameObject.tag == "CarMenu")
+        {
+            Debug.Log("COLLISION HAPPENED!!!");
+
+        }
+
+    }
+
+}
+
+
