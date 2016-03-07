d0d1cbaaa6d3880dd31a2fe4fa8351cefdfbc60b
Federico Soncini <fsonc002@gold.ac.uk>
2016-03-07 12:54:19 +0000

    Added Player 1 UI test
    Script can move object around

---
+++ Assets/Scripts/P1Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P1Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        HandleMovement();
+
+	}
+
+    void HandleMovement ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) { transform.Translate(2, 0, 0); }
+        if (Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(-2, 0, 0); }
+        if(Input.GetKey(KeyCode.UpArrow)) { transform.Translate(0, 2, 0); }
+        if(Input.GetKey(KeyCode.DownArrow)) { transform.Translate(0, -2, 0); }
+    }
+}
