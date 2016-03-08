965bd6bee82bb41384d2bcdd4046d2c76e8e1592
Federico Soncini <fsonc002@gold.ac.uk>
2016-03-08 12:40:36 +0000

    Implemented Go! access to level 1
    go no longer "button"

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,12 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
 
-	public void LoadLevel(string name)
+    public Player p;
+    public Text TextColor;
+
+    void Start()
     {
+        TextColor = GameObject.FindWithTag("Go").GetComponent<Text>();
+        p = GameObject.FindWithTag("PlayerMenu").GetComponent<Player>();
+    }
+
+
+    void Update()
+    {
+        if (p.notSelected == false)
+        {
+            TextColor.color = Color.green;
+
+            if (Input.GetKey(KeyCode.B))
+            {
+
+                LoadLevel(name);
+            }
+        }
+
+
+    }
+
+    public void LoadLevel(string name)
+    {
+        name = "main2";
         SceneManager.LoadScene(name);
 
     }
 }
-
