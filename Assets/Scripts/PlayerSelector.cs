9d2cdcf0e1eb3c07afcad177f05374bbad536a28
Federico Soncini <fsonc002@gold.ac.uk>
2016-03-09 14:54:05 +0000

    added some test to PlayerSelector script
    still needs polishing

---
+++ Assets/Scripts/PlayerSelector.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+public class PlayerSelector : MonoBehaviour {
+
+
+    public Dropdown dropdown;
+    public Button playButton;
+
+    void Awake()
+    {
+        playButton.onClick.AddListener(new UnityAction(SaveNumberToPlayerPrefs));
+    }
+
+    private void SaveNumberToPlayerPrefs()
+    {
+        int numberToSave;
+        int selectedIndex = dropdown.value;
+        if (int.TryParse(dropdown.options[selectedIndex].text, out numberToSave))
+        {
+            PlayerPrefs.SetInt("SavedNumber", numberToSave);
+        }
+        else
+        {
+            Debug.LogError("Chose option is no number: " + dropdown.options[selectedIndex].text);
+        }
+    }
+}
+    public void LoadLevel(string name)
+    {
+
+        SceneManager.LoadScene(name);
+
+    }
+
+
+    //create script for drop down menus that holds and returns number of players and computers 
+    //max players + computers = 4.
+    //public void onValueChanged()
+    //{
+
+    //}
+
+}
