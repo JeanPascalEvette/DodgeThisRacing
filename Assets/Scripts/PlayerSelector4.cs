cae9906f4fd920c4bf7817b150671daa412fb4d8
Elio de Berardinis <eliodeb@gmail.com>
2016-03-12 13:42:09 +0000

    importing fede's scripts

---
+++ Assets/Scripts/PlayerSelector4.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelector4 : MonoBehaviour
+{
+
+    public Text t;
+    int switch_case = 0;
+
+    void switch_reset()
+    {
+        switch_case = 0;
+    }
+
+    public void player_selector()
+    {
+        
+        switch_case++;
+
+        if (switch_case < 4)
+        {
+            switch (switch_case)
+            {
+                case 1:
+                    //Debug.Log("player mode selected");
+                    t.text = "P1";
+                    break;
+                case 2:
+                    //Debug.Log("cpu mode selected");
+                    t.text = "CPU";
+                    break;
+                default:
+                    //Debug.Log("null mode selected");
+                    t.text = " ";
+                    switch_case = 0;
+                    break;
+            }
+            Debug.Log(switch_case);
+        }
+
+        else
+        {
+            Debug.Log(switch_case);
+            switch_reset();
+        }
+        
+    }
+}
