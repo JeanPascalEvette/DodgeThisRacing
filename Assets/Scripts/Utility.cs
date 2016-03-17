38f335da5343a330083621d0425ba5f29e9d1423
BaggioPereira <baggio_111111@hotmail.com>
2016-03-17 07:42:11 +0000

    Early Utility script

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utility : MonoBehaviour {
+
+    int ID;
+    Vector3 velocity;
+    Vector3 position;
+    int timeSteps;
+    Vector3[] stepPositions;
+
+    void NewUtility(CarState state, int steps)
+    {
+        ID = state.myUniqueID;
+        timeSteps = steps;
+        velocity = state.myVelocity;
+        position = state.myPosition;
+    }
+
+    Vector3 CalculateFinalPos()
+    {
+        Vector3 finalPos;
+        finalPos = (velocity * timeSteps) + position;
+        return finalPos;
+    }
+
+    Vector3[] CalculateSteps()
+    {
+        for(int i = 0; i< timeSteps; i++)
+        {
+            stepPositions[i] = (velocity * i) + position;
+        }
+        return stepPositions;
+    }    
+}
