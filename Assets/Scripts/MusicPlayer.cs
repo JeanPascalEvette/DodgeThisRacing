1e86642d149cf12cead801a94e9ce7ca76a2d714
Federico Soncini <fsonc002@gold.ac.uk>
2016-04-25 12:16:39 +0100

    added start scene graphics

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+    //initialize to null because at first there will be no defined call instance
+    static MusicPlayer instance = null;
+
+    void Awake () {
+        if (instance != null) {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing!");
+        } else {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
