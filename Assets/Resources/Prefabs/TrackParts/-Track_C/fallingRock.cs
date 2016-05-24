5a43a67bf27739dadc152c75133d930943cab2bb
skarregaard89 <skarregaard89@gmail.com>
2016-05-24 15:39:48 +0100

    Martins prefab

---
+++ Assets/Resources/Prefabs/TrackParts/-Track_C/fallingRock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fallingRock : MonoBehaviour {
+
+    Transform rock;
+
+    // Use this for initialization
+    void Start () {
+
+       rock =  this.transform.GetChild(0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        rock.gameObject.SetActive(true);
+        print("HERE!!!!");
+    }
+}
---
+++ Assets/Resources/Prefabs/TrackParts/-Track_C/testMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.GetComponent<Rigidbody>().AddForce(0, 0, 10);
+	
+	}
+}
